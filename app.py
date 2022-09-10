c2c0f024994ebb7a001c187b2f825394952ce873
sanyamjain0315 <111227515+sanyamjain0315@users.noreply.github.com>
2022-09-10 12:48:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+import joblib
+
+model=joblib.load('Emotion Detector')
+
+st.title('Text Emotion Detector')
+x=st.text_input('Enter text....')
+y=model.predict([x])
+if st.button('Predict'):
+  st.success(op[0])
